866b7936e507b185749c7065468b284493599113
Aashay-Thakur <88266722+Aashay-Thakur@users.noreply.github.com>
2023-11-24 14:57:53 +0530

    Update install.sh

--- agent/install.sh
+++ agent/install.sh
@@ -1,30 +1,19 @@
+#!/bin/bash
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-apt-get update -y
-apt-get upgrade -y
-if ! command -v node &> /dev/null
+if ! command -v python3 &> /dev/null
 then
     echo "Installing Node"
     apt-get install nodejs -y
-    if ! command -v node &> /dev/null
-    then
-        echo "Node was not successfully installed"
-        exit
-    fi
 else 
-    echo "Node is already installed"
+    echo "node is already installed"
 fi
 if ! command -v npm &> /dev/null
 then
-    echo "Installing npm"
+    echo "Installing python3"
     apt-get install npm -y
-    if ! command -v npm &> /dev/null
-    then
-        echo "npm was not successfully installed"
-        exit
-    fi
 else 
     echo "npm is already installed"
 fi
@@ -32,29 +21,34 @@ if ! command -v python3 &> /dev/null
 then
     echo "Installing python3"
     apt-get install python3 -y
-    if ! command -v python3 &> /dev/null
-    then
-        echo "python3 was not successfully installed"
-        exit
-    fi
 else 
     echo "python3 is already installed"
 fi
-source npm install
-python3 -m venv venv
+npm install localtunnel -y
+apt install python3.10-venv -y
+if [ ! -d "$venv" ]
+then
+	python3 -m venv venv
+fi
 source venv/bin/activate
-python3 pip install -r requirements.txt
+if test -f "requirements.txt"
+then
+	pip install -r requirements.txt
+else
+	echo "Please include the requirements.txt file"
+	exit
+fi
 if ! command -v gnome-terminal &> /dev/null
 then
     echo "Installing gnome-terminal"
     apt-get install gnome-terminal -y
-    if ! command -v gnome-terminal &> /dev/null
-    then
-        echo "gnome-terminal was not successfully installed"
-        exit
-    fi
 else 
     echo "gnome-terminal is already installed"
 fi
-gnome-terminal -- bash -c "npm run expose; exec bash"
+if ! command -v dbus-launch &> /dev/null
+then
+	echo "Installing dbus-launch"
+	apt install dbus-x11 -y
+fi
+sudo -u mca gnome-terminal -- bash -c "npx lt -s test -p 3001; exec bash -i"
 python3 agent.py
