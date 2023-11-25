57d404e10618b1bb7f1d33ded35745c47bd43a93
Aashay-Thakur <thakur.aashay@gmail.com>
2023-11-25 13:40:46 +0530

    Add dependency checks and download necessary files

--- agent/install.bat
+++ agent/install.bat
@@ -1,20 +1,77 @@
 echo off
+echo ---Checking for dependencies---
+where curl --version >nul 2>nul
+if errorlevel 1 (
+    echo curl is not installed
+    echo Please install curl
+    echo Press any key to exit...
+    pause >nul
+    exit
+) else (
+    echo curl is installed
+)
+where node -version >nul 2>nul
+if errorlevel 1 (
+    echo node is not installed
+    echo Please install node.js
+    echo Press any key to exit
+    pause >nul
+    exit
+) else (
+    echo node is installed
+)
+where npm -version >nul 2>nul
+if errorlevel 1 (
+    echo npm is not installed
+    echo looks like npm was not installed with nodejs
+    echo reinstalling nodejs should fix this
+    echo Press any key to exit...
+    pause >nul
+    exit
+) else (
+    echo npm is installed
+)
+where python -version >nul 2>nul
+if errorlevel 1 (
+    echo python is not installed
+    echo Please install python
+    echo Press any key to exit
+    pause >nul
+    exit
+) else (
+    echo python is installed
+)
 if exist package.json (
     echo package.json exists
 ) else (
     echo package.json does not exist
-    echo Please include package.json in the same directory as install.bat
-    echo If you do not have package.json, please run npm init -y
+    echo downloading package.json
+    curl https://raw.githubusercontent.com/Aashay-Thakur/Prototype/main/agent/package.json -O
+)
+if errorlevel 0 (
+    goto :continue
+) else (
+    echo failed to download package.json
+    echo looks like package.json was not downloaded successfully
+    echo creating package.json
+    call npm init -y   
+)
+if errorlevel 1 (
+    echo failed to create package.json
     echo Press any key to exit
     pause >nul
     exit
-)
-if exist node_modules\ (
-    echo node_modules exists
 ) else (
-    echo node_modules does not exist
-    echo Installing node_modules
-    call npm install
+    echo package.json created
+)
+:continue
+where npx lt --version >nul 2>nul
+if errorlevel 1 (
+    echo localtunnel is not installed
+    echo installing localtunnel
+    call npm install localtunnel
+) else (
+    echo localtunnel is installed
 )
 if exist venv\ (
     echo venv exists
@@ -23,18 +80,37 @@ if exist venv\ (
     echo creating python virtual environment
     call python -m venv ./venv
 )
-echo activating python virtual environment
-call ./venv/Scripts/activate.bat
+if errorlevel 1 (
+    echo failed to activate python virtual environment
+    echo Press any key to exit
+    pause >nul
+    exit
+) else (
+    echo python virtual environment created
+)
+echo downloading requirements.txt
+curl https://raw.githubusercontent.com/Aashay-Thakur/Prototype/main/agent/requirements.txt -O
 if exist requirements.txt (
     echo requirements.txt exists
 ) else (
     echo requirements.txt does not exist
-    echo Please include requirements.txt in the same directory as install.bat
-    echo If you do not have requirements.txt, please run pip freeze > requirements.txt
-    echo Press any key to exit
+    echo looks like requirements.txt was not downloaded successfully
+    echo please include requirements.txt in the same directory as install.bat
+    echo Press any key to exit...
     pause >nul
     exit
 )
+echo activating python virtual environment
+call ./venv/Scripts/activate.bat
+if errorlevel 1 (
+    echo failed to activate python virtual environment
+    echo Press any key to exit
+    pause >nul
+    exit
+) else (
+    echo python virtual environment activated
+)
+echo checking if requirements.txt is up to date
 pip freeze > temp.txt
 fc /b temp.txt requirements.txt > nul
 if errorlevel 1 (
@@ -45,9 +121,19 @@ if errorlevel 1 (
     echo requirements.txt is up to date
 )
 del temp.txt
-echo Installation complete
-echo starting new localtunnel session in a new window
-start cmd /C npm run expose
+echo downloading agent.py
+curl https://raw.githubusercontent.com/Aashay-Thakur/Prototype/main/agent/agent.py -O
+if exist agent.py (
+    echo downloaded agent.py successfully
+) else (
+    echo agent.py does not exist
+    echo looks like agent.py was not downloaded successfully
+    echo Press any key to exit...
+    pause >nul
+    exit
+)
+echo ---Installation complete---
+echo starting a localtunnel session in a new window
+start cmd /C "npm run expose"
 echo starting agent.py
 python agent.py
-```
--- agent/install.sh
+++ agent/install.sh
@@ -1,54 +1,205 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-if ! command -v python3 &> /dev/null
-then
-    echo "Installing Node"
-    apt-get install nodejs -y
-else 
-    echo "node is already installed"
-fi
-if ! command -v npm &> /dev/null
-then
-    echo "Installing python3"
-    apt-get install npm -y
-else 
-    echo "npm is already installed"
-fi
-if ! command -v python3 &> /dev/null
-then
-    echo "Installing python3"
-    apt-get install python3 -y
-else 
-    echo "python3 is already installed"
-fi
-npm install localtunnel -y
-apt install python3.10-venv -y
-if [ ! -d "$venv" ]
-then
-	python3 -m venv venv
-fi
-source venv/bin/activate
-if test -f "requirements.txt"
-then
-	pip install -r requirements.txt
+
+setlocal
+
+YELLOW_BG='\033[43m'
+RED='\033[0;31m'
+RESET='\033[0m'
+
+echo "---Checking for dependencies---"
+
+if command -v curl &>/dev/null; then
+    echo "curl is installed"
 else
-	echo "Please include the requirements.txt file"
-	exit
+    echo "curl is not installed"
+    echo "Please install curl"
+    echo "Press any key to exit..."
+    read -n 1 -s
+    exit
 fi
-if ! command -v gnome-terminal &> /dev/null
-then
-    echo "Installing gnome-terminal"
-    apt-get install gnome-terminal -y
-else 
-    echo "gnome-terminal is already installed"
+
+if command -v node &>/dev/null; then
+    echo "node is installed"
+else
+    echo "node is not installed"
+    echo "installing nodejs"
+    sudo apt-get install nodejs -y
+    if [ $? -eq 1 ]; then
+        echo "Failed to install nodejs"
+        echo "Press any key to exit..."
+        read -n 1 -s
+        exit
+    else
+        echo "nodejs installed"
+    fi
 fi
-if ! command -v dbus-launch &> /dev/null
-then
-	echo "Installing dbus-launch"
-	apt install dbus-x11 -y
+
+if command -v npm &>/dev/null; then
+    echo "npm is installed"
+else
+    echo "npm is not installed"
+    echo "installing npm"
+    apt-get install npm -y
+    if [ $? -eq 1 ]; then
+        echo "Failed to install npm"
+        echo "Press any key to exit..."
+        read -n 1 -s
+        exit
+    else
+        echo "npm installed"
+    fi
 fi
-sudo -u mca gnome-terminal -- bash -c "npx lt -s test -p 3001; exec bash -i"
+
+if command -v python3 &>/dev/null; then
+    echo "python3 is installed"
+else
+    echo "python3 is not installed"
+    echo "installing python3"
+    apt-get install python3 -y
+    if [ $? -eq 1 ]; then
+        echo "Failed to install python3"
+        echo "Press any key to exit..."
+        read -n 1 -s
+        exit
+    else
+        echo "python3 installed"
+    fi
+fi
+
+if command -v pip &>/dev/null; then
+    echo "pip is installed"
+else
+    echo "pip is not installed"
+    echo "installing pip"
+    apt-get install python3-pip -y
+    if [ $? -eq 1 ]; then
+        echo "Failed to install pip"
+        echo "Press any key to exit..."
+        read -n 1 -s
+        exit
+    else
+        echo "pip installed"
+    fi
+fi
+
+if [ -f package.json ]; then
+    echo "package.json exists"
+else
+    echo "package.json does not exist"
+    echo "Downloading package.json"
+    wget -O package.json https://raw.githubusercontent.com/Aashay-Thakur/Prototype/main/agent/package.json
+fi
+
+if [ $? -eq 0 ]; then
+    echo "package.json downloaded successfully"
+else
+    echo "Failed to download package.json"
+    echo "Looks like package.json was not downloaded successfully"
+    echo "Creating package.json"
+    npm init -y
+    if [ $? -eq 1 ]; then
+        echo "Failed to create package.json"
+        echo "Press any key to exit..."
+        read -n 1 -s
+        exit
+    else
+        echo "package.json created"
+    fi
+fi
+
+if command -v npx localtunnel &>/dev/null; then
+    echo "localtunnel is installed"
+else
+    echo "localtunnel is not installed"
+    echo "Installing localtunnel"
+    npm install localtunnel
+fi
+
+if [ -d venv ]; then
+    echo "venv exists"
+else
+    echo "venv does not exist"
+    echo "Creating python virtual environment"
+    apt install python3.10-venv -y
+    if [ $? -eq 1 ]; then
+        echo "Failed to install python3.10-venv"
+        echo "Press any key to exit..."
+        read -n 1 -s
+        exit
+    fi
+    python3 -m venv ./venv
+fi
+
+if [ $? -eq 1 ]; then
+    echo "Failed to create python virtual environment"
+    echo "Press any key to exit"
+    read -n 1 -s
+    exit
+else
+    echo "Python virtual environment created"
+fi
+
+echo "Downloading requirements.txt"
+wget -O requirements.txt https://raw.githubusercontent.com/Aashay-Thakur/Prototype/main/agent/requirements.txt
+
+if [ $? -eq 0  ]; then
+    echo "requirements.txt downloaded successfully"
+else
+    echo "Looks like requirements.txt was not downloaded successfully"
+    if [ -e requirements.txt ]; then
+        echo "found an old requirements.txt"
+    else
+        echo "requirements.txt does not exist"
+        echo "Creating requirements.txt"
+        touch requirements.txt
+        echo "requirements.txt created"
+        
+        echo -e "${YELLOW_BG}${RED}Warning${RESET}: This is an empty requirements.txt file"
+        echo "This might cause errors"
+        # echo "Press any key to continue..."
+        # read -n 1 -s
+    fi
+fi
+
+echo "Activating python virtual environment"
+source venv/bin/activate
+
+if [ $? -eq 1 ]; then
+    echo "Failed to activate python virtual environment"
+    echo "Press any key to exit"
+    read -n 1 -s
+    exit
+else
+    echo "Python virtual environment activated"
+fi
+
+if [ ! -s requirements.txt ]; then
+    echo "requirements.txt is empty"
+    echo -e "${YELLO_BG}${RED}Warning${RESET}: Are you sure you want to continue?"
+    echo -e "${YELLO_BG}${RED}Warning${RESET}: Requirements.txt is empty"
+    echo "Please replace the file with a valid requirements.txt file"
+    echo "Press any key to continue..."
+    read -n 1 -s
+fi
+
+pip install -r requirements.txt
+
+echo "Downloading agent.py"
+wget -O agent.py https://raw.githubusercontent.com/Aashay-Thakur/Prototype/main/agent/agent.py
+
+if [ $? -eq 0 ]; then
+    echo "Downloaded agent.py successfully"
+else
+    echo "agent.py does not exist"
+    echo "Looks like agent.py was not downloaded successfully"
+    echo "Press any key to exit..."
+    read -n 1 -s
+    exit
+fi
+
+echo "---Installation complete---"
+echo "Starting a localtunnel session in a new window"
+# sudo -u mca gnome-terminal -- bash -c "npx lt -s test -p 3001; exec bash -i"
+gnome-terminal -- bash -c "npm run expose; exec bash -i"
+echo "Starting agent.py"
 python3 agent.py
